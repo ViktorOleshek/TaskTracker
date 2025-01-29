5ee49436363822b7bd8e471777aeee34be852ea3
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2025-01-29 16:19:01 +0200

    Add NuGet packages and extension for DI

---
+++ Persistence/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistence.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
---
+++ WebApi/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Database;
+using Microsoft.OpenApi.Models;
+
+namespace WebApi.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApi(this IServiceCollection services)
+        {
+            services
+                .AddEndpointsApiExplorer()
+                .AddSwaggerGen(c =>
+                    {
+                        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Task Tracker API", Version = "v1" });
+                    })
+                .AddControllers()
+                ;
+
+            return services;
+        }
+    }
+}
