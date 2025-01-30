51ecba6eac2e1703dc75810d819cf92534d39325
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2025-01-31 01:59:36 +0200

    Add user registration handling

---
+++ Application/Commands/AddUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.Commands;
+public class AddUserCommand : IRequest<Guid>
+{
+    public string Login { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
