820f17d4b04a6995ad302aa479e6ce98f69ac2b7
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2025-01-31 11:40:32 +0200

    Add authentification controller

---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Application.Commands;
+using Application.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public UserController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(AddUserCommand command)
+    {
+        try
+        {
+            var userId = await _mediator.Send(command);
+            return Ok(new { UserId = userId });
+        }
+        catch (InvalidOperationException ex)
+        {
+            return BadRequest(new { Error = ex.Message });
+        }
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginUserQuery query)
+    {
+        try
+        {
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return Unauthorized(new { Error = ex.Message });
+        }
+    }
+}
