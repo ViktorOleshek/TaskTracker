326232224d36909d57619d8c67bae3899596891d
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2025-02-04 09:09:57 +0200

    Add new repository and roles constant for authorization

--- Domain/Abstraction/Repositories/IProjectMemberRepository.cs
+++ Domain/Abstraction/Repositories/IProjectMemberRepository.cs
@@ -1,8 +1,12 @@
 ﻿using Domain.Entities;
 
 namespace Domain.Abstraction.Repositories;
-public interface IProjectMemberRepository : IRepository<ProjectMember>
+public interface IProjectMemberRepository
 {
-    Task<ProjectMember?> GetRoleByUserAndProjectAsync(Guid userId, Guid projectId);
+    System.Threading.Tasks.Task CreateAsync(ProjectMember entity);
+    Task<ProjectMember?> GetAsync(Guid userId, Guid projectId);
+    Task<Role?> GetRoleAsync(Guid userId, Guid projectId);
+    Task<int> UpdateAsync(ProjectMember entity);
     Task<int> DeleteAsync(ProjectMember entity);
+    Task<IEnumerable<ProjectMember>> GetAllByProjectIdAsync(Guid projectId);
 }
--- Persistence/Repositories/ProjectMemberRepository.cs
+++ Persistence/Repositories/ProjectMemberRepository.cs
@@ -4,23 +4,68 @@ using Domain.Abstraction.Repositories;
 using Domain.Entities;
 
 namespace Persistence.Repositories;
-internal class ProjectMemberRepository : BaseRepository<ProjectMember>, IProjectMemberRepository
+internal class ProjectMemberRepository : IProjectMemberRepository
 {
+    private readonly ISqlConnectionFactory _connectionFactory;
     public ProjectMemberRepository(ISqlConnectionFactory connectionFactory)
-        : base(connectionFactory)
-    { }
+    {
+        _connectionFactory = connectionFactory;
+    }
 
-    public async Task<ProjectMember?> GetRoleByUserAndProjectAsync(Guid userId, Guid projectId)
+    public async System.Threading.Tasks.Task CreateAsync(ProjectMember entity)
+    {
+        using var connection = _connectionFactory.CreateConnection();
+        await connection.ExecuteAsync(
+        @"INSERT INTO ProjectMember(UserId, ProjectId, RoleId)
+            VALUES(@UserId, @ProjectId, @RoleId)",
+            new { UserId = entity.UserId, ProjectId = entity.ProjectId, RoleId = entity.RoleId });
+    }
+
+    public async Task<ProjectMember?> GetAsync(Guid userId, Guid projectId)
     {
         using var connection = _connectionFactory.CreateConnection();
         return await connection.QuerySingleOrDefaultAsync<ProjectMember>(
-            "SELECT * FROM UserProjects WHERE UserId = @UserId AND ProjectId = @ProjectId",
+            @"SELECT * FROM [Projects] p
+            JOIN [UserProjects] up ON p.Id = up.ProjectId
+            JOIN [Users] u ON u.Id = up.UserId
+            WHERE p.Id = @ProjectId AND up.UserId = @UserId",
             new { UserId = userId, ProjectId = projectId });
+    }
+
+    public async Task<Role?> GetRoleAsync(Guid userId, Guid projectId)
+    {
+        using var connection = _connectionFactory.CreateConnection();
+        return await connection.QuerySingleOrDefaultAsync<Role>(
+            @"SELECT r.* FROM [Roles] r
+            JOIN [UserProjects] up ON r.Id = up.RoleId
+            WHERE up.UserId = @UserId AND up.ProjectId = @ProjectId",
+            new { UserId = userId, ProjectId = projectId });
+    }
+
+    public async Task<int> UpdateAsync(ProjectMember entity)
+    {
+        using var connection = _connectionFactory.CreateConnection();
+        return await connection.ExecuteAsync(
+            @"UPDATE UserProjects
+            SET RoleId = @RoleId
+            WHERE UserId = @UserId AND ProjectId = @ProjectId",
+            new { entity.RoleId, entity.UserId, entity.ProjectId });
     }
 
     public async Task<int> DeleteAsync(ProjectMember entity)
     {
         using var connection = _connectionFactory.CreateConnection();
-        return await connection.DeleteAsync(entity);
+        return await connection.ExecuteAsync(
+            @"DELETE FROM UserProjects
+            WHERE UserId = @UserId AND ProjectId = @ProjectId",
+            new { entity.UserId, entity.ProjectId });
+    }
+
+    public async Task<IEnumerable<ProjectMember>> GetAllByProjectIdAsync(Guid projectId)
+    {
+        using var connection = _connectionFactory.CreateConnection();
+        return await connection.QueryAsync<ProjectMember>(
+            @"SELECT * FROM UserProjects WHERE ProjectId = @ProjectId",
+            new { ProjectId = projectId });
     }
 }
