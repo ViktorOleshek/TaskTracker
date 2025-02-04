07de79a28edb040c35098ee8b40e2748920cebbc
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2025-02-04 09:02:59 +0200

    Add projectMember Dto

---
+++ Domain/DTOs/ProjectMemberDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.DTOs;
+
+public class ProjectMemberDto
+{
+    public Guid UserId { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public Guid RoleId { get; set; }
+    public string RoleName { get; set; } = string.Empty;
+}
---
+++ Domain/DTOs/ProjectMemberRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.DTOs;
+
+public class ProjectMemberRequest
+{
+    public Guid UserId { get; set; }
+    public Guid RoleId { get; set; }
+};
